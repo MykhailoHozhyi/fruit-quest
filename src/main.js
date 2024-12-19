0ac2b6b94294094ffeb2cafbd0cdffaeca309eeb
Mykhailo Hozhyi <gozhenky@gmail.com>
2024-12-19 16:12:52 +0200

    make structure add fonts

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './js/header';
+import './js/mobile-menu';
+import './js/hero';
+import './js/gameplay-gallery';
+import './js/about-us';
+import './js/reviews';
+import './js/video-gallery';
+import './js/footer';
+
+import './js/privacy-policy';
+import './js/terms-conditions';

5475ddd406382bfc19019b045b5ebe6969150ed5
Kseniya Semenenko <165210222+KseniyaSemenenko@users.noreply.github.com>
2024-12-20 13:02:58 +0200

    add-img-footer-privacy

--- src/js/video-gallery.js
+++ src/js/video-gallery.js
@@ -1,15 +1,15 @@
-const video = document.getElementById('myVideo');
-const videoContainer = document.getElementById('videoContainer');
+// const video = document.getElementById('myVideo');
+// const videoContainer = document.getElementById('videoContainer');
 
-// Додаємо обробники подій для відео
-video.addEventListener('play', () => {
-  videoContainer.style.backgroundColor = 'rgba(0, 0, 0, 0)'; // Непрозорий фон
-});
+// // Додаємо обробники подій для відео
+// video.addEventListener('play', () => {
+//   videoContainer.style.backgroundColor = 'rgba(0, 0, 0, 0)'; // Непрозорий фон
+// });
 
-video.addEventListener('pause', () => {
-  videoContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // Напівпрозорий фон
-});
+// video.addEventListener('pause', () => {
+//   videoContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // Напівпрозорий фон
+// });
 
-video.addEventListener('ended', () => {
-  videoContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // Напівпрозорий фон після завершення
-});
+// video.addEventListener('ended', () => {
+//   videoContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // Напівпрозорий фон після завершення
+// });

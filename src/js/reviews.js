dadb834bbd614ba4f6d1b35248cf5086890da905
Kseniya Semenenko <165210222+KseniyaSemenenko@users.noreply.github.com>
2024-12-21 12:40:23 +0200

    add-terms-updated-all

---
+++ src/js/reviews.js
@@ -0,0 +1,8 @@
+// import Swiper from 'swiper';
+
+// export const swiper = new Swiper('.swiper', {
+//   pagination: {
+//     el: '.swiper-pagination',
+//     dynamicBullets: true,
+//   },
+// });
